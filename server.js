c1ac3d55aaeac45924ba6cc7b938114c95e70885
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-07-31 14:08:47 +0700

    add header for express respond

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ class Server {
 
   constructor() {
     this.app = express();
+
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
     this.app.use('/', (req, res, next) => {
       console.log(req.url);
       next();
